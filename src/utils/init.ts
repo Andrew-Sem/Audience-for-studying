e2ed348e9b93ec87104fe41f7b44f87bd5e4f47a
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-11 22:12:00 +0300

    add renderer options for shadows

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -1,4 +1,4 @@
-import {Camera, PerspectiveCamera, Renderer, Scene, WebGLRenderer} from "three";
+import {Camera, PCFSoftShadowMap, PerspectiveCamera, Renderer, Scene, WebGLRenderer} from "three";
 
 type InitReturn = {
     scene: Scene
@@ -10,6 +10,8 @@ export const init = ():InitReturn => {
     const scene = new Scene();
     const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new WebGLRenderer();
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = PCFSoftShadowMap;
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
