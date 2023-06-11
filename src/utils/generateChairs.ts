b41052a3d7d7a7795bb46ce7fdf8afa222d2f4b5
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-12 00:20:49 +0300

    reduce chairs count, remove source chair

--- src/utils/generateChairs.ts
+++ src/utils/generateChairs.ts
@@ -6,9 +6,10 @@ export const generateChairs = async (scene: Scene) => {
     const chair = await loadObject("/chair.glb", scene)
     chair.position.y = -sizes.height / 2 + 0.01
     chair.scale.set(0.5, 0.5, 0.5)
+    scene.remove(chair)
 
-    for(let i = -2; i < 5; i++){
-        for(let j = -2; j < 3; j++){
+    for(let i = -2; i < 4; i++){
+        for(let j = -1.25; j <= 1.25; j+=1.25){
             const clonedChair = chair.clone()
             clonedChair.position.x = j
             clonedChair.position.z = i
