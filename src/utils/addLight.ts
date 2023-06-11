3afa3077792acd9b9d8bdbc621514431ff9f7803
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-11 23:25:11 +0300

    nice lamps version

--- src/utils/addLight.ts
+++ src/utils/addLight.ts
@@ -10,50 +10,34 @@ export const addLight = (scene: Scene) => {
     directionalLight.castShadow = true
     scene.add(directionalLight);
 
-    const width = 0.8;
-    const height = 0.8;
-    const intensity = 1;
-
-    for (let i = 0; i < 4; i++) {
-        const rectLight = new RectAreaLight(0xffffff, intensity, width, height);
-        rectLight.position.set(1, 1.44, i * 1.5);
-        rectLight.lookAt(1, -2, i * 1.5);
-        scene.add(rectLight)
-        const rectLightHelper = new RectAreaLightHelper(rectLight);
-        rectLight.add(rectLightHelper);
-    }
-
-    for (let i = 0; i < 4; i++) {
-        const rectLight = new RectAreaLight(0xffffff, intensity, width, height);
-        rectLight.position.set(-1, 1.44, i * 1.5);
-        rectLight.lookAt(-1, -2, i * 1.5);
-        scene.add(rectLight)
-        const rectLightHelper = new RectAreaLightHelper(rectLight);
-        rectLight.add(rectLightHelper);
-    }
-
-    function addSpotLight(
+    // функция для добавления лампочек
+    const addLamp = (
         from: { x: number, y: number, z: number },
         to: { x: number, y: number, z: number },
-        angle: number, penumbra: number
-    ) {
+        angle: number, penumbra: number, width: number, height: number
+    ) => {
+        // свет прожектора для лампочки
         const spotLight = new SpotLight(0xffffff, 0.01);
         spotLight.position.set(from.x, from.y, from.z);
-        spotLight.target.position.set(to.x, to.y, to.y);
+        spotLight.target.position.set(to.x, to.y, to.z);
         spotLight.angle = angle; // Угол конуса света
         spotLight.penumbra = penumbra; // Мягкость краев
         spotLight.castShadow = true;
         scene.add(spotLight);
         scene.add(spotLight.target);
+
+        // сама лампочка
+        const rectLight = new RectAreaLight(0xffffff, 0.8, width, height);
+        rectLight.position.set(from.x, from.y, from.z);
+        rectLight.lookAt(to.x, to.y, to.z);
+        scene.add(rectLight)
+        const rectLightHelper = new RectAreaLightHelper(rectLight);
+        rectLight.add(rectLightHelper);
     }
 
-    // Добавление SpotLight для каждого RectAreaLight
-    for (let i = 0; i < 4; i++) {
-        addSpotLight({x: 1, y: 1.44, z: i * 1.5}, {x: -1, y: -2, z: i * 1.5}, Math.PI / 4, 0.2);
-    }
-
-    for (let i = 0; i < 4; i++) {
-        addSpotLight({x: -1, y: 1.44, z: i * 1.5}, {x: 1, y: -2, z: i * 1.5}, Math.PI / 4, 0.2);
+    for (let i = -2; i < 4; i++) {
+        addLamp({x: 1, y: 1.44, z: i * 1.44}, {x: 1, y: -2, z: i * 1.44}, Math.PI / 3, 0.5, 0.8, 0.8,)
+        addLamp({x: -1, y: 1.44, z: i * 1.44}, {x: -1, y: -2, z: i * 1.44}, Math.PI / 3, 0.5, 0.8, 0.8)
     }
 
 }
