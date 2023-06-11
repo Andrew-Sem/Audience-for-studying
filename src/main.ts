38df256765b916363502e02496bb546ff07a0520
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-11 18:40:03 +0300

    add loadModel func, add chair model

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {addLights} from "./lights.ts";
+import {loadModel} from "./loadModel.ts";
 
 // Create the scene, camera, and renderer
 const scene = new THREE.Scene();
@@ -8,7 +9,9 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+camera.position.z = 2;
+
+loadModel("/chair.glb", scene)
 
 addLights(scene)
 
