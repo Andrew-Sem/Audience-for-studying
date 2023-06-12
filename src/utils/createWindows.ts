a75ceea850571874f7eb1e9458a0789afaa53e15
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-12 16:35:42 +0300

    add back windows

--- src/utils/createWindows.ts
+++ src/utils/createWindows.ts
@@ -4,10 +4,17 @@ import {substractWall} from "./substractWall.ts";
 import {Mesh} from "three";
 
 export const createWindows = (walls: Mesh[]) => {
-    const windows:Mesh[] = []
+    const rightWindows:Mesh[] = []
     for(let i = -2; i < 3; i++){
         const window = addWindow(0.1, 1.5, 1, sizes.width/2, 0, i * 2, 0.2);
-        windows.push(window)
+        rightWindows.push(window)
     }
-    substractWall(walls[1], windows)
+    substractWall(walls[1], rightWindows)
+
+    const backWindows:Mesh[] = []
+    for(let i = -1; i < 2; i+=2){
+        const window = addWindow(1, 1.5, 0.1, i, 0, sizes.depth/2, 0.2);
+        backWindows.push(window)
+    }
+    substractWall(walls[2], backWindows)
 }
