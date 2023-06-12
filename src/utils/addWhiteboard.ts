bf2266fd3bd00850cbb72f5d71e661b5c990c41b
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-12 16:13:14 +0300

    fix z position for whiteboard

--- src/utils/addWhiteboard.ts
+++ src/utils/addWhiteboard.ts
@@ -19,7 +19,7 @@ export const addWhiteboard = () => {
     const imagePlane = new Mesh(imageGeometry, imageMaterial);
 
     // Установка позиции для плоскости с изображением
-    imagePlane.position.set(0, 0, -sizes.depth / 2+1);
+    imagePlane.position.set(0, 0, -sizes.depth / 2 + 0.1);
 
     // Добавление плоскости с изображением в сцену
     scene.add(imagePlane);
