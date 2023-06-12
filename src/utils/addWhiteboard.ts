ea6dc81299539d5cd92a427b4538843aea735db8
Andrew-Sem <avvyayvayva@gmail.com>
2023-06-12 16:46:47 +0300

    update styles for whiteboard

--- src/utils/addWhiteboard.ts
+++ src/utils/addWhiteboard.ts
@@ -13,13 +13,13 @@ export const addWhiteboard = () => {
     const imageMaterial = new MeshBasicMaterial({map: texture});
 
     // Создание геометрии для плоскости с изображением
-    const imageGeometry = new PlaneGeometry(sizes.width / 1.8, sizes.height / 1.8, );
+    const imageGeometry = new PlaneGeometry(sizes.width / 2, sizes.height / 2, );
 
     // Создание меша с плоскостью и применение материала
     const imagePlane = new Mesh(imageGeometry, imageMaterial);
 
     // Установка позиции для плоскости с изображением
-    imagePlane.position.set(0, 0, -sizes.depth / 2 + 0.1);
+    imagePlane.position.set(-0.8, 0, -sizes.depth / 2 + 0.1);
 
     // Добавление плоскости с изображением в сцену
     scene.add(imagePlane);
